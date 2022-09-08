ab1857f8e5edfb32529a0dee24a4577e80037e5c
Ahmed Ashraf <57802370+Ahmed2398@users.noreply.github.com>
2022-09-08 15:44:11 +0200

    update about and game,reg,soft

--- src/js/master.js
+++ src/js/master.js
@@ -83,6 +83,8 @@ function openPage(pageName, elmnt, color) {
 // Get the element with id="defaultOpen" and click on it
 
 document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpens").click();
+
 
 
 
