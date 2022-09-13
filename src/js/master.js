15ceb4a803c9676b3f6f2f03e27a1aadbbb7c42e
Ahmed Ashraf <57802370+Ahmed2398@users.noreply.github.com>
2022-09-13 16:34:10 +0200

    v.2

--- src/js/master.js
+++ src/js/master.js
@@ -26,7 +26,7 @@ function themeChange(event) {
 
 
 // Home Header  Nav-link Target
-document.querySelector(".nav-target").addEventListener("click", function (e) {
+document.querySelector(".nav-target").addEventListener("lick", function (e) {
   e.preventDefault();
 
   // MAtching strategy
@@ -61,31 +61,4 @@ function topFunction() {
 }
 // END
 
-// tab Game content 
-
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++){
-    tabcontent[i].style.display = "none"
-  }
-    // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-    // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
-// Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
-}
-// Get the element with id="defaultOpen" and click on it
-
-document.getElementById("defaultOpen").click();
-document.getElementById("defaultOpens").click();
-
-
-
-
-
+   
